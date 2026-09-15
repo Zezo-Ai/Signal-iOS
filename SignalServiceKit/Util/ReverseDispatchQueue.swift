e1bb97cc0e0a26d7bdecae8b9a9144f32b35051c
Max Radermacher <max@signal.org>
2026-09-14 23:00:22 -0500

    Simplify ReverseDispatchQueue

--- SignalServiceKit/Util/ReverseDispatchQueue.swift
+++ SignalServiceKit/Util/ReverseDispatchQueue.swift
@@ -7,48 +7,22 @@ import Foundation
 
 // This is intended to be a drop-in replacement for DispatchQueue
 // that processes its queue in reverse order.
-@objc
-public class ReverseDispatchQueue: NSObject {
+public class ReverseDispatchQueue {
 
-    private let label: String
     private let serialQueue: DispatchQueue
 
-    @objc
-    public convenience init(label: String) {
-        self.init(label: label, qos: .unspecified, autoreleaseFrequency: .inherit)
-    }
-
     public init(label: String, qos: DispatchQoS, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency) {
-        self.label = label
         serialQueue = DispatchQueue(label: label, qos: qos, autoreleaseFrequency: autoreleaseFrequency)
-
-        super.init()
     }
 
     public typealias WorkBlock = () -> Void
 
-    private class Item {
-        let workBlock: WorkBlock
-        let index: UInt64
+    // This property should only be accessed on serialQueue.
+    private var workBlocks = [WorkBlock]()
 
-        init(workBlock: @escaping WorkBlock, index: UInt64) {
-            self.workBlock = workBlock
-            self.index = index
-        }
-    }
-
-    // These properties should only be accessed on serialQueue.
-    private var items = [Item]()
-    private var indexCounter: UInt64 = 0
-
-    @objc
-    public func async(workBlock: @escaping WorkBlock) {
+    public func async(execute work: @escaping WorkBlock) {
         serialQueue.async {
-            self.indexCounter += 1
-            let index = self.indexCounter
-            let item = Item(workBlock: workBlock, index: index)
-            self.items.append(item)
-
+            self.workBlocks.append(work)
             self.process()
         }
     }
@@ -58,12 +32,11 @@ public class ReverseDispatchQueue: NSObject {
             // Note that we popLast() so that we process
             // the queue in the _reverse_ order from
             // which it was enqueued.
-            guard let item = self.items.popLast() else {
+            guard let work = self.workBlocks.popLast() else {
                 // No enqueued work to do.
                 return
             }
-            item.workBlock()
-
+            work()
             self.process()
         }
     }
